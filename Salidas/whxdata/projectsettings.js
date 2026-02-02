d5a4d2cd62061b21f446fa537ac757d297291d65
saraHovagimian <shovagimian@bas.com.ar>
2026-02-02 16:46:23 -0300

    UP-0069 020226

--- Salidas/whxdata/projectsettings.js
+++ Salidas/whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("BASCSHTML/bienvenido.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.html'));
 rh.consts('START_FILEPATH', encodeURI('index.html'));
-rh.consts('HELP_ID', '31f925bc-bddb-4900-8f9b-6a225e12c248' || 'preview');
+rh.consts('HELP_ID', '1b4460f0-6815-4179-b36e-172fc047d2c0' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
